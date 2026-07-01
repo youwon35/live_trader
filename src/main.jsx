fe335c44faf1170cc7e1eea6d980019a73d16b21
cntdividebyzero <youwon35@naver.com>
2026-07-01 12:35:33 +0900

    Import shared UI patterns

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "../../../packages/design/action-feedback.css";
+import "../../../packages/design/ui-patterns.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
