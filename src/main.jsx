dec6d6cd914761dd93bf962788ed761f3ce0309a
cntdividebyzero <youwon35@naver.com>
2026-07-14 02:28:00 +0900

    style: adopt shared operator console

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from "./App.jsx";
 import "./styles.css";
 import "../../../packages/design/action-feedback.css";
 import "../../../packages/design/ui-patterns.css";
+import "../../../packages/design/program-console.css";
+import "../../../packages/design/layout-editing.css";
+import { installLayoutEditingSupport } from "../../../packages/design/layout-editing.js";
+
+installLayoutEditingSupport({ program: "live-trader" });
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
